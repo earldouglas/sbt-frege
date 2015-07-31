5b3f72e632acbc8ca34c5595ab7c6d7161110fb9
James Earl Douglas <james@earldouglas.com>
2015-07-30 17:08:43 -0700

    Use source generation instead compilation
    This uses `sourceGenerators` instead of overriding `compile`.  The
    result is *.java* files produced by fregec, that are subsequently
    compiled via sbt into the standard target directory.
    
    Fixes #7
    Fixes #2

---
+++ src/sbt-test/sbt-frege/mixed/src/main/java/com/earldouglas/helloworld/HelloFrege.java
@@ -0,0 +1,7 @@
+package com.earldouglas.helloworld;
+
+public class HelloFrege {
+  public static void main(String[] args) {
+    HelloWorld.main(args); // Will not compile unless Frege code is compiled
+  }
+}
